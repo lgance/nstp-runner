698c45125e38b84b75c7587d42aab9f9211f22ac
jaeuk.lee <dlwodnrsms@naver.com>
2020-08-26 19:36:03 +0900

    last fixed

--- src/common/upload.ts
+++ src/common/upload.ts
@@ -1,12 +1,13 @@
-// const path = require('path');
-// const FormData = require('form-data');
-// const fs = require('fs');
+import fs from 'fs';
+import FormData from 'form-data';
+import axios from 'axios';
+import path from 'path';
 // const axios = require('axios');
 
-// /** define config variable  */
+/* //? define config variable  */
 // const uploadFilePath = path.resolve(__dirname,'test-png2.png');
 // const uploadUrl = 'http://127.0.0.1/upload';
-// const returnTimeout = 5000;
+const returnTimeout = 5000;
 
 // sendReport(uploadUrl,uploadFilePath)
 // .then(res=>{
@@ -17,21 +18,21 @@
 // })
 
 
-// function sendReport(uploadUrl,uploadFilePath){
-//   return new Promise((resolve,reject)=>{
-//     if(fs.existsSync(uploadFilePath)){
-//       const stream = fs.createReadStream(uploadFilePath);
-//       const form = new FormData();
-//       form.append('dump',stream);
-//       axios.post(uploadUrl,form,{
-//         headers:form.getHeaders(),
-//         timeout:returnTimeout
-//       })
-//       .then(res=>{resolve(res.data)})
-//       .catch(err=>{reject(false);})
-//     }
-//     else{
-//       reject('not Exist File');
-//     }
-//   })
-// }
+function sendReport(uploadUrl:string,uploadFilePath:string){
+  return new Promise((resolve,reject)=>{
+    if(fs.existsSync(uploadFilePath)){
+      const stream = fs.createReadStream(uploadFilePath);
+      const form = new FormData();
+      form.append('dump',stream);
+      axios.post(uploadUrl,form,{
+        headers:form.getHeaders(),
+        timeout:returnTimeout
+      })
+      .then(res=>{resolve(res.data)})
+      .catch(err=>{reject(false);})
+    }
+    else{
+      reject('not Exist File');
+    }
+  })
+}
