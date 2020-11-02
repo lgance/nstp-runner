ecac94a5c1d13c52ce960e4c0273a8760fb9ab34
jaeuk.lee <dlwodnrsms@naver.com>
2020-11-03 01:56:34 +0900

    legacy file delete and not used remark delete

--- src/index.ts
+++ src/index.ts
@@ -40,29 +40,3 @@ const main = async ()=>{
   }
 }
 main();
-
-
-// const main = async () => {
-//   const browser = await Puppeteer.getBrowser({ isHeadless: false })
-//   if (browser) {
-//       // const page = await browser.newPage()
-//       const context = await browser.createIncognitoBrowserContext();
-
-//       const page = await context.newPage();
-
-//       // * An example of crawling a page with CloudFlare applied.
-//       Logger.debug('🚧  Crawling in progress...')
-
-//       // const url = 'https://namu.wiki/w/Cloudflare'
-//       // const url = `http:console.ncloud.com`;
-//       const url = 'https://console.ncloud.com/';
-//       await Puppeteer.goto(page, url)
-//       await page.screenshot({ path: 'example.png' })
-
-//       Logger.debug('🚧  Crawling is complete.')
-//       Logger.debug('🚧  Exit the Puppetier...')
-//       browser.close()
-//   }
-// }
-
-// main()
